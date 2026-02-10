d5c00cf173b92d6578fa9a1e44c04507fbee77c1
Linda Franco <Linda_Franco96@hotmail.com>
2026-02-10 17:21:59 -0600

    Home inicial

--- src/App.js
+++ src/App.js
@@ -1,52 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import Contact from './components/Contact';
+import React from 'react';
+import styled from 'styled-components'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import AboutMe from './components/AboutMe'
+import Skills from './components/Skills'
+import Services from './components/Services'
+import Projects from './components/Projects'
+import Footer from './components/Footer'
+import Section from './assets/styles/Section'
 
 function App() {
-  const [proyectos, setProyectos] = useState([]);
-
-  useEffect(() => {
-    //fetch('http://localhost:8080/api/proyectos')
-    //fetch('https://api-portfolio-linda.onrender.com/api/proyectos')
-    const url = `${process.env.REACT_APP_API_URL}/api/proyectos`;
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        console.log("Lista recibida:", data);
-        setProyectos(data);
-      })
-      .catch(error => console.error("Error:", error));
-  }, []);
-
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'Arial' }}>
-      <h1>📂 Mis Proyectos (Dinámicos)</h1>
-      <hr />
+    <BrowserRouter>
+      <MainContainer>
+        <Header />
 
-      <div style={{ display: 'flex', justifyContent: 'center', gap: '20px', flexWrap: 'wrap' }}>
-        
-        {proyectos.map((p) => (
-          
-          <div key={p.id} style={{ 
-              border: '1px solid #666', 
-              borderRadius: '10px', 
-              padding: '20px', 
-              width: '250px', 
-              boxShadow: '0 4px 8px rgba(0,0,0,0.1)' 
-            }}>
-            
-            <h3 style={{ color: 'blue' }}>{p.nombre}</h3>
-            <p>{p.descripcion}</p>
-            <small>ID: {p.id}</small>
-          
-          </div>
-
-        ))}
-
-        <Contact />
-
-      </div>
-    </div>
+        <Main>
+          <Switch>
+            <Route path="/about" component={AboutMe} />
+            <Route path="/skills" component={Skills} />
+            <Route path="/services" component={Services} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/" exact={true}>
+              <Section primary>
+                <Home />
+              </Section>
+              <Section >
+                <AboutMe />
+              </Section>
+              <Section primary>
+                <Skills />
+              </Section>
+              <Section>
+                <Services />
+              </Section>
+              <Section primary>
+                <Projects />
+              </Section>
+            </Route>
+          </Switch>
+        </Main>
+        <Footer />
+      </MainContainer>
+    </BrowserRouter>
   );
 }
+
+const MainContainer = styled.div`
+  width: 100%;
+`
+
+const Main = styled.main`
+  width: 100%;
+`
 
 export default App;
